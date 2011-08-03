dc2e94fa9adb0dbcaff7efb658b5512919762694
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2011-08-03 01:44:24 -0400

    added "real" config poll (ie, doesn't use static data anymore)

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -10,11 +10,20 @@ import re
 # FUNCIONES generales para Auth2DB
 import functions
 
+# Import ConfigObj
+sys.path.insert(1,"/usr/share/auth2db/modules/")
+from configobj import ConfigObj
 
-CONFIG_HOST = "localhost"
-CONFIG_USER = "root"
-CONFIG_PASS = ""
-CONFIG_DB = "auth2db"
+# Load configuration from /etc/auth2db/auth2db.conf
+CONFIG_PATH = "/etc/auth2db/"
+config = ConfigObj(CONFIG_PATH+'auth2db.conf')
+config_filters = ConfigObj(CONFIG_PATH+"filters.conf")
+
+# Grab configuration information from config file
+CONFIG_HOST = config['CONFIG_HOST']
+CONFIG_DB = config['CONFIG_DB']
+CONFIG_USER = config['CONFIG_USER']
+CONFIG_PASS = config['CONFIG_PASS']
 
 def MonitorData(idview):
 	'''Mueve la DATA de la tabla MAIN(log_*) a JUNK [o de JUNK a MAIN]'''
